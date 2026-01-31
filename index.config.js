559b365b8b019ee1978df23a6c3305d24cc31d6c
Toos <151813251+xymn2023@users.noreply.github.com>
2026-01-31 15:26:10 +0800

    Update index.config.js

--- index.config.js
+++ index.config.js
@@ -2,9 +2,9 @@ var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
+var __export = (target, 所有) => {
+  for (var 名字 in 所有)
+    __defProp(target, 名字, { get: 所有[名字], enumerable: true });
 };
 var __copyProps = (to, from, except, desc) => {
   if (from && typeof from === "object" || typeof from === "function") {
@@ -19,7 +19,7 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 // src/index.config.js
 var index_config_exports = {};
 __export(index_config_exports, {
-  default: () => index_config_default
+  默认: () => index_config_default
 });
 module.exports = __toCommonJS(index_config_exports);
 var index_config_default = {
@@ -62,98 +62,98 @@ var index_config_default = {
   },
    danmu: {
     urls: [
-           { address: "https://logdanmu.dpdns.org", name: "默认1" },
-           { address: "https://fjj0417.dpdns.org/87654321", name: "默认2" }, 
-           { address: "https://dm.stardm.us.kg:443/87654321", name: "默认3" }, 
-           { address: "https://danmu.14812910.xyz/87654321", name: "默认4" }, 
-           { address: "https://313236.xyz/87654321", name: "默认5" }
+           { address: "https://logdanmu.dpdns.org", 名字: "默认1" },
+           { address: "https://fjj0417.dpdns.org/87654321", 名字: "默认2" }, 
+           { address: "https://dm.stardm.us.kg:443/87654321", 名字: "默认3" }, 
+           { address: "https://danmu.14812910.xyz/87654321", 名字: "默认4" }, 
+           { address: "https://313236.xyz/87654321", 名字: "默认5" }
           ],
     autoPush: true
   },
  t4: {list: [
-    {name: "✈️关注TG频道@stymei",
+    {名字: "主站",
       address: "http://zhangqun1818.serv00.net:6628/?spider=独播库"},
-    {name: "👖裤佬丨电视丨直播",
+    {名字: "丨电视丨直播",
       address: "http://zhangqun1818.serv00.net/zh/2242.php"},
-    {name: "👖裤佬丨瓜子丨影视",
+    {名字: "丨瓜子丨影视",
       address: "https://newtv.ggff.net/guazi?token=MRdKOtZ4"},
-    {name: "👖裤佬丨瓜子丨备用",
+    {名字: "丨瓜子丨备用",
       address: "https://catbox.n13.club/ai/瓜子影视.php"},
-    {name: "👖裤佬丨永乐丨影视",
+    {名字: "丨永乐丨影视",
       address: "https://newtv.ggff.net/yongle?token=MRdKOtZ4"},
-    {name: "👖裤佬丨欧乐丨影视",
+    {名字: "丨欧乐丨影视",
       address: "https://newtv.ggff.net/oule?token=MRdKOtZ4"},
-    {name: "👖裤佬丨荐片丨影视",
+    {名字: "丨荐片丨影视",
       address: "https://newtv.ggff.net/jianpian?token=MRdKOtZ4"},
-    {name: "👖裤佬丨ikbot丨影视",
+    {名字: "丨ikbot丨影视",
       address: "https://newtv.ggff.net/ikanbot?token=MRdKOtZ4"},
-    {name: "👖裤佬丨剧吧丨影视",
+    {名字: "丨剧吧丨影视",
       address: "https://newtv.ggff.net/jubaba?token=MRdKOtZ4"},
-    {name: "👖裤佬丨奇优丨影视",
+    {名字: "丨奇优丨影视",
       address: "https://newtv.ggff.net/uvod?token=MRdKOtZ4"},
-    {name: "👖裤佬丨爱瓜丨影视",
+    {名字: "丨爱瓜丨影视",
       address: "https://newtv.ggff.net/aigua?token=MRdKOtZ4"},
-    {name: "👖裤佬丨3Q丨影视",
+    {名字: "丨3Q丨影视",
       address: "http://zhangqun1818.serv00.net:6628/?spider=3q影视"},
-    {name: "👖裤佬丨红果丨短剧",
+    {名字: "丨红果丨短剧",
       address: "https://catbox.n13.club/ai/红果短剧.php"},
-    {name: "👖裤佬丨旺旺丨短剧",
+    {名字: "丨旺旺丨短剧",
       address: "http://zhangqun1818.serv00.net/ww.php"},
-    {name: "👖裤佬丨Emby丨WebDAV",
+    {名字: "丨Emby丨WebDAV",
       address: "http://zhangqun1818.serv00.net:6628/?spider=emby"},
-    {name: "👖裤佬丨凤凰丨FM",
+    {名字: "丨凤凰丨FM",
       address: "http://zhangqun1818.serv00.net:6628/?spider=凤凰fm"},
-    {name: "👖裤佬丨采集丨聚合",
+    {名字: "丨采集丨聚合",
       address: "http://zhangqun1818.serv00.net/cj/cjjh.php"},
-    {name: "👖裤佬丨JS丨聚合",
+    {名字: "丨JS丨聚合",
       address: "http://zhangqun1818.serv00.net/js.php"},
-    {name: "👖裤佬丨PY丨聚合",
+    {名字: "丨PY丨聚合",
       address: "http://zhangqun1818.serv00.net/py.php"},
-    {name: "👖裤佬丨PHP丨聚合",
+    {名字: "丨PHP丨聚合",
       address: "http://zhangqun1818.serv00.net/php.php"}
 ]},
   cms: {list: [
-{name: "👖🔞裤佬丨麻花丨采集",
+{名字: "🔞丨麻花丨采集",
 address: "https://19q.cc/api.php/provide/vod"},
-{name: "👖🔞裤佬丨杏吧丨采集",
+{名字: "🔞丨杏吧丨采集",
 address: "https://xingba111.com/api.php/provide/vod/?ac=list"},
-{name: "👖🔞裤佬丨越南丨采集",
+{名字: "🔞丨越南丨采集",
 address: "https://vnzyz.com/api.php/provide/vod"},
-{name: "👖🔞裤佬丨细胞丨采集",
+{名字: "🔞丨细胞丨采集",
 address: "https://www.xxibaozyw.com/api.php/provide/vod"},
-{name: "👖🔞裤佬丨91AV丨采集",
+{名字: "🔞丨91AV丨采集",
 address: "https://91av.cyou/api.php/provide/vod"},
-{name: "👖🔞裤佬丨小鸡丨采集",
+{名字: "🔞丨小鸡丨采集",
 address: "https://api.xiaojizy.live/provide/vod"},
-{name: "👖🔞裤佬丨大奶丨采集",
+{名字: "🔞丨大奶丨采集",
 address: "https://apidanaizi.com/api.php/provide/vod"},
-{name: "👖🔞裤佬丨仓库丨采集",
+{名字: "🔞丨仓库丨采集",
 address: "https://hsckzy888.com/api.php/provide/vod/from/hsckm3u8/at/json"},
-{name: "👖🔞裤佬丨玉兔丨采集",
+{名字: "🔞丨玉兔丨采集",
 address: "https://apiyutu.com/api.php/provide/vod"},
-{name: "👖🔞裤佬丨奶香丨采集",
+{名字: "🔞丨奶香丨采集",
 address: "https://naixxzy.com/api.php/provide/vod"},
-{name: "👖🔞裤佬丨乐播丨采集",
+{名字: "🔞丨乐播丨采集",
 address: "https://lbapi9.com/api.php/provide/vod"},
-{name: "👖🔞裤佬丨幸源丨采集",
+{名字: "🔞丨幸源丨采集",
 address: "https://xzybb1.com/api.php/provide/vod"}
 ]},
   alist: [
     {
-      name: "🐉神族九帝",
+      名字: "🐉神族九帝",
       server: "https://alist.shenzjd.com"
     },
     {
-      name: "💢repl",
+      名字: "💢repl",
       server: "https://ali.liucn.repl.co"
     }
   ],
   color: [
     {
-      light: {
+      亮: {
         bg: "https://i2.100024.xyz/2024/01/13/pptcej.webp",
         bgMask: "0x50ffffff",
-        primary: "0xff446732",
+        主要: "0xff446732",
         onPrimary: "0xffffffff",
         primaryContainer: "0xffc5efab",
         onPrimaryContainer: "0xff072100",
@@ -184,10 +184,10 @@ address: "https://xzybb1.com/api.php/provide/vod"}
         inversePrimary: "0xffaad291",
         surfaceTint: "0xff446732"
       },
-      dark: {
+      暗: {
         bg: "https://i2.100024.xyz/2024/01/13/pptg3z.webp",
         bgMask: "0x50000000",
-        primary: "0xffaad291",
+        主要: "0xffaad291",
         onPrimary: "0xff173807",
         primaryContainer: "0xff2d4f1c",
         onPrimaryContainer: "0xffc5efab",
